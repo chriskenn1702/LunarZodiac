b357c1aff7db2ee70a393abbee6572af6626da27
Christopher Kennedy <kennednu@bc.edu>
2023-01-30 18:01:09 -0500

    Initial Commit

---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Christopher Kennedy on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
