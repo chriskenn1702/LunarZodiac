50f7d7bec50ba10d829aa3750cda27115284b5be
Christopher Kennedy <kennednu@bc.edu>
2023-01-30 18:23:18 -0500

    Lunar Zodiac w/o Array

--- LunarZodiac/ContentView.swift
+++ LunarZodiac/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var year = 2023
+    @State var imageNum = 3
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Text(String(year))
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            Image("image"+String(imageNum))
+                .resizable()
+                .scaledToFit()
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack{
+                Button("<"){
+                    year-=1
+                    imageNum-=1
+                    if imageNum == -1{
+                        imageNum = 11
+                    }
+                }
+                
+                Spacer()
+                
+                Button(">"){
+                    year+=1
+                    imageNum+=1
+                    if imageNum == 12{
+                        imageNum = 0
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+            .font(.largeTitle)
+            .fontWeight(.black)
         }
         .padding()
     }
